9b2d60d0b4091d0b6c008cfa0d8906affa597446
ub1213go <ub1213go@gmail.com>
2021-07-19 19:44:47 +0800

    1

---
+++ Lab_ClaimsIdentity/Models/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Lab_ClaimsIdentity.Models
+{
+    public partial class UserInfo
+    {
+        public string Uid { get; set; }
+        public string Email { get; set; }
+        public string PassWord { get; set; }
+        public string Role { get; set; }
+        public DateTime? SignDate { get; set; }
+        public string Class { get; set; }
+        public string Salt { get; set; }
+    }
+}
