a491ec52378630bbd0116439fc609ffb1bce7eb6
Yuu <yy556023@gmail.com>
2021-07-21 16:29:46 +0800

    2021/07/21 註冊登錄完成 豪

---
+++ WorkoutHunter/WorkoutHunter/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutHunter.Models
+{
+    public class User
+    {
+        public string UID { get; set; }
+        [Required]
+        [RegularExpression("[a-zA-Z0-9_]*@.*")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("[a-zA-Z0-9_&%$^#?=]*")]
+        public string Password { get; set; }
+    }
+}
