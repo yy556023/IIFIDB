268536bfad500d0822defb7052793766398f0b9a
Yuu <yy556023@gmail.com>
2021-07-21 17:08:35 +0800

    SQL程式碼備份

---
+++ SQL/Insert/skill.sql
@@ -0,0 +1,7 @@
+insert into skill Values
+('肘擊', 220, 60, 'xxxx.jpg'),
+('後旋踢', 300, 40, 'xxxx.jpg'),
+('劈掛', 100, 30, 'xxxx.jpg'),
+('山突', 250, 15, 'xxxx.jpg'),
+('刺拳', 150, 5, 'xxxx.jpg')
+GO
---
+++ SQL/Insert/user_status.sql
@@ -0,0 +1,7 @@
+insert into user_status Values
+('kVcPI4sAGy',  66, 5, 93, 5),
+('atz5mBCnph',  28, 58, 86, 3),
+('ARRQNF9BCN',  16, 20, 7, 7),
+('QTi0PFrQLi',  17, 20, 87, 3),
+('pR6KRtwCmN',  52, 10, 10, 6)
+GO
