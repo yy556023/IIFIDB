af524369f6159050b0691503e92889117b2b43f8
ub1213go <ub1213go@gmail.com>
2021-07-23 14:46:49 +0800

    更改SQL程式insert資料UID使其與excel資料同步

--- SQL/Insert/user_status.sql
+++ SQL/Insert/user_status.sql
@@ -1,7 +1,8 @@
 insert into user_status Values
-('kVcPI4sAGy',  66, 5, 93, 5),
-('atz5mBCnph',  28, 58, 86, 3),
-('ARRQNF9BCN',  16, 20, 7, 7),
-('QTi0PFrQLi',  17, 20, 87, 3),
-('pR6KRtwCmN',  52, 10, 10, 6)
+('B5b165PNvx',  66, 5, 93, 5),
+('HA4uwwsvgv',  28, 58, 86, 3),
+('qO8tGnvha7',  16, 20, 7, 7),
+('wiPwIWPQNm',  17, 20, 87, 3),
+('YCmOMpKwCV',  52, 10, 10, 6)
 GO
+
