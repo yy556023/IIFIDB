52a7a3d9fa31700739637f2a0dac8523ef3f52df
ub1213go <ub1213go@gmail.com>
2021-07-24 18:16:25 +0800

    修改技能資料表

--- SQL/Insert/skill.sql
+++ SQL/Insert/skill.sql
@@ -1,10 +1,11 @@
+use WorkoutHunter
 insert into skill Values
 ('星爆氣流斬', 0, 60, 1,'xxxx.jpg'),
 ('三千世界', 0, 60, 3, 'xxxx.jpg'),
 ('水之呼吸', 0, 60, 5, 'xxxx.jpg'),
-('最後的月牙天衝', 0, 60, 7, 'xxxx.jpg'),
-('肘擊', 0, 60, 9, 'xxxx.jpg'),
-('認真毆打', 0, 60, 11, 'xxxx.jpg'),
-('神聖彗星反射力量', 0, 60, 13, 'xxxx.jpg'),
-('雙重之極限', 0, 60, 8, 'xxxx.jpg')
+('雙重之極限', 0, 60, 7, 'xxxx.jpg'),
+('月牙天衝', 0, 60, 9, 'xxxx.jpg'),
+('螺旋丸', 0, 60, 11, 'xxxx.jpg'),
+('猛虎', 0, 60, 13, 'xxxx.jpg'),
+('底特律爆風', 0, 60, 15, 'xxxx.jpg')
 GO
