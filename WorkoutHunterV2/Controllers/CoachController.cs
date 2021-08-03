28d495874e66e9622b851f959452366a450a3883
ub1213go <ub1213go@gmail.com>
2021-08-03 15:16:45 +0800

    整合完成v1.0

--- WorkoutHunterV2/Controllers/CoachController.cs
+++ WorkoutHunterV2/Controllers/CoachController.cs
@@ -19,15 +19,15 @@ namespace WorkoutHunterV2.Controllers
 
         public async Task<IActionResult> Index()
         {
-            // 找出身分為學生的資料
-            //var query = _context.UserInfos.Where(o => o.Role == "S");
-            //// 得到教練的UID
-            //string UID = User.Claims.Single(p => p.Type == "ID").Value;
-            //// 找出登錄這位教練的學生
-            //var query2 = query.Where(o => o.PT == UID);
+            //找出身分為學生的資料
+           var query = _context.UserInfos.Where(o => o.Role == "S");
+            // 得到教練的UID
+            string UID = User.Claims.Single(p => p.Type == "ID").Value;
+            // 找出登錄這位教練的學生
+            var query2 = query.Where(o => o.PT == UID);
 
-            //return View(await query2.ToListAsync());
-            return View(await _context.UserInfos.ToListAsync());
+            return View(await query2.ToListAsync());
+            //return View(await _context.UserInfos.ToListAsync());
         }
     }
 }
